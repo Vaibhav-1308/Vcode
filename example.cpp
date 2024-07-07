2aca55ec8a4f9eed4d17bc9f782a570627a6015d
Vaibhav-1308 <vaibhavnrupnarayan30@gmail.com>
2024-07-07 12:10:25 +0530

    just for practice

--- example.cpp
+++ example.cpp
@@ -1,18 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int32_t main(){
-
-    int n;
-    cin>>n;
-    
-    int choco = n;
-
-    while(n>=3){
-        int rap = n / 3;
-        choco = choco + rap;
-        n = rap + n%3;
-    }
+int main(){
 
     cout<<choco<<endl;
     return 0;
